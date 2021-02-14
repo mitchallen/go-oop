dc310400aec24af9588237d31f50268019b841fe
Mitch Allen <github@mitchallen.com>
2021-02-14 15:30:21 -0500

    file cleanup

--- go-oop.go
+++ go-oop.go
@@ -5,28 +5,21 @@ import (
 )
 
 type Person struct {
-
-	// defining struct variables
 	first string
 	last  string
 	age   int
 }
 
 func (person Person) info() {
-
 	fmt.Printf("%s  %s.", person.first, person.last)
-	fmt.Printf("\n is  %d year old.\n", person.age)
+	fmt.Printf("\n is  %d year(s) old.\n", person.age)
 }
 
 // main function
 func main() {
-
 	p1 := Person{"John", "Smith", 57}
-
 	p1.info()
-
 	p1.first = "Jane"
 	p1.age = 56
-
 	p1.info()
 }
